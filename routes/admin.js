9d29b6f76988ef0d5bd4e2fcb3849ba877df9b7a
abu <dhooomy2188@gmail.com>
2025-07-30 19:22:58 +0300

    ghfg

--- routes/admin.js
+++ routes/admin.js
@@ -1,47 +1,17 @@
 const express = require('express');
-const { body } = require('express-validator');
-const tasksControllers = require('../controller/admin');
-const tasksFilter = require('../controller/admin-filter');
-const isAuth = require('../middlware/is-auth');
-const multer = require('multer');
-const path = require('path');
+const tasksControllers = require('../controller/admin')
+const tasksFilter = require('../controller/admin-filter')
+const isAuth = require('../middlware/is-auth')
+const route = express.Router();
+const {body} = require('express-validator')
 
-const router = express.Router();
 
-// إعداد رفع الصورة
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // تأكد إن الفولدر موجود
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
+route.get('/all-tasks' ,isAuth,tasksFilter.filter   , tasksControllers.getTasks)
+route.get('/user-tasks/:id' ,isAuth,tasksFilter.filter   , tasksControllers.getUserTasks)
+route.post('/add-task' ,isAuth, body('title').trim().isLength({max:255 , min:5})  , tasksControllers.createTask)
+route.put('/edit-task/:id' ,isAuth, body('title').trim().isLength({max:255 , min:5})  , tasksControllers.editTask)
+route.delete('/delete-task/:id',isAuth, tasksControllers.deleteTask)
+route.get('/task/:id',isAuth, tasksControllers.taskDetails)
+route.put('/complete',isAuth, tasksControllers.completeTask)
 
-const upload = multer({ storage: storage });
-
-// الراوتات
-router.get('/all-tasks', isAuth, tasksFilter.filter, tasksControllers.getTasks);
-router.get('/user-tasks/:id', isAuth, tasksFilter.filter, tasksControllers.getUserTasks);
-
-router.post(
-  '/add-task',
-  isAuth,
-  upload.single('image'), // ← لازم دا عشان الصورة
-  body('title').trim().isLength({ min: 5, max: 255 }),
-  tasksControllers.createTask
-);
-
-router.put(
-  '/edit-task/:id',
-  isAuth,
-  upload.single('image'), // لو حابب تعدل الصورة كمان
-  body('title').trim().isLength({ min: 5, max: 255 }),
-  tasksControllers.editTask
-);
-
-router.delete('/delete-task/:id', isAuth, tasksControllers.deleteTask);
-router.get('/task/:id', isAuth, tasksControllers.taskDetails);
-router.put('/complete', isAuth, tasksControllers.completeTask);
-
-module.exports = router;
+module.exports = route

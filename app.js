4407005ae0f92e6b910099065eb21c4c36852286
abu <dhooomy2188@gmail.com>
2025-07-30 17:51:54 +0300

    cv

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors'); // 👈
+
 const tasksRoute = require("./routes/admin")
 const authRoute = require("./routes/auth")
 const app = express()
@@ -11,50 +13,47 @@ const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerDocument = require('./swagger.json');
 require("dotenv").config();
 const port = process.env.PORT;
+
+// CORS middleware
+app.use(cors()); // 👈 Enable CORS globally
+
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null,  file.originalname);
+    cb(null, file.originalname);
   }
 });
 
 const fileFilter = (req, file, cb) => {
-    if (
-      file.mimetype === 'image/png' ||
-      file.mimetype === 'image/jpg' ||
-      file.mimetype === 'image/jpeg'
-      ) {
-        cb(null, true);
-      } else {
-        cb(null, false);
-      }
-    };
-    
-    // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-   // const specs = swaggerJsDoc(options);
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    app.use(bodyParser.json()); // application/json
-  app.use(
-    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-  );
-  app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use((req , res , next) => {
-    res.setHeader("Access-Control-Allow-Origin" , "*")
-    res.setHeader("Access-Control-Allow-Methods" , "GET , POST , PUT , PATCH , DELETE")
-    res.setHeader("Access-Control-Allow-Headers" , "Content-Type , Authorization") 
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
-    next()
-})
-app.use('/tasks' , tasksRoute)
-app.use('/auth' , authRoute)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/tasks', tasksRoute)
+app.use('/auth', authRoute)
 
 app.use((error, req, res, next) => {
-    const status = error.statusCode || 500;
-    const message = error.message;
-    res.status(status).json({ message: message });
-  });
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
 DBConcction(() => {
-    app.listen(port)
-})
+  app.listen(port)
+});
